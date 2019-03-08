0a3e5e11f65338282f1a60040f4d8122cd056706
wujingpeng <wujingpeng@youzan.com>
2019-03-08 12:59:15 +0800

    💪 WIP:
    Fibonacci Number

---
+++ src/com/kingkong/leetcode/FibonacciNumber/Solution.java
@@ -0,0 +1,36 @@
+package com.kingkong.leetcode.FibonacciNumber;
+
+public class Solution {
+    public int fib01(int N) {
+        if (N == 0) {
+            return 0;
+        }
+
+        if (N == 1) {
+            return 1;
+        }
+
+        int p = 0, q = 1;
+        int sum = 0;
+
+        for (i = 2; i < N; i++) {
+            sum = p + q;
+            p = q;
+            q = sum;
+        }
+
+        return sum;
+    }
+
+    public int fib02(int N) {
+        if (N == 0) {
+            return 0;
+        }
+
+        if (N == 1) {
+            return 1;
+        }
+
+        return fib02(N - 1) + fib02(N - 2);
+    }
+}
