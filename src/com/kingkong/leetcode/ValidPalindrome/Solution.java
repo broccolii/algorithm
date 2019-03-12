6b1b5b27b585ffc3e84da4095e5e9eedb1e558c9
wujingpeng <wujingpeng@youzan.com>
2019-03-12 08:23:17 +0800

    💪 WIP:
    Valid Palindrome

---
+++ src/com/kingkong/leetcode/ValidPalindrome/Solution.java
@@ -0,0 +1,27 @@
+package com.kingkong.leetcode.ValidPalindrome;
+
+public class Solution {
+
+    private boolean isAlphanumeric(char c) {
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+    }
+
+    private boolean isEqualIgnoreCase(char a, char b) {
+        if (a >= 'A' && a <= 'Z') a += 32;
+        if (b >= 'A' && b <= 'Z') b += 32;
+        return a == b;
+    }
+
+
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) return true;
+
+        int i = 0, j = s.length() - 1;
+        for (; i < j; ++i, --j) {
+            while (i < j && !isAlphanumeric(s.charAt(i))) ++i;
+            while (i < j && !isAlphanumeric(s.charAt(j))) --j;
+            if (i < j && !isEqualIgnoreCase(s.charAt(i), s.charAt(j))) return false;
+        }
+        return true;
+    }
+}
