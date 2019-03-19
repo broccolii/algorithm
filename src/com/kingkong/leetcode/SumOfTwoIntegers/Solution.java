ede2ccfea381de216572e7f8bea055e6f2257883
wujingpeng <wujingpeng@youzan.com>
2019-03-19 13:44:13 +0800

    💪 WIP:
    update SumOfTwoIntegers

--- src/com/kingkong/leetcode/SumOfTwoIntegers/Solution.java
+++ src/com/kingkong/leetcode/SumOfTwoIntegers/Solution.java
@@ -7,8 +7,8 @@ public class Solution {
 
     public int getSum02(int a, int b) {
         while (b != 0) {
-            sum = a ^ b;
-            carry = (a & b) << 1;
+            int sum = a ^ b;
+            int carry = (a & b) << 1;
 
             a = sum;
             b = carry;
