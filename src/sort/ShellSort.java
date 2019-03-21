ec8aeb974ddc1216ed58af5bf504df4c9e0a209a
wujingpeng <wujingpeng@youzan.com>
2019-03-21 14:47:48 +0800

    💪 WIP:
    ShellSort

---
+++ src/sort/ShellSort.java
@@ -0,0 +1,22 @@
+package sort;
+
+public class ShellSort {
+    public void sort(int[] arr) {
+        int section = arr.length / 2;
+        int j;
+        int temp;
+
+        while (section >= 1) {
+            for (int i = section; i < arr.length; i++) {
+                temp = arr[i];
+                j = i - section;
+                while (j >= 0 && arr[j] > temp) {
+                    arr[j + section] = arr[j];
+                    j = j - section;
+                }
+                arr[j + section] = temp;
+            }
+            section /= 2;
+        }
+    }
+}
