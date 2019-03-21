2a549245995270369c04e2012d332dcfbe1750d9
wujingpeng <wujingpeng@youzan.com>
2019-03-21 14:03:43 +0800

    💪 WIP:
    BubbleSort

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,17 @@
+package sort;
+
+public class BubbleSort {
+    public void sort(int[] arr) {
+        int lens = arr.length;
+
+        for (int i = 0; i < lens - 1; i++) {
+            for (int j = lens - 1; j > i; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
