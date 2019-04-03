65bfb213ca9bb35f92b9c593cb3a59d3547f81ab
wujingpeng <wujingpeng@youzan.com>
2019-04-03 23:23:24 +0800

    💪 WIP:
    fix bug

--- src/com/kingkong/leetcode/LowestCommonAncestorOfABinaryTree/Solution.java
+++ src/com/kingkong/leetcode/LowestCommonAncestorOfABinaryTree/Solution.java
@@ -1,5 +1,8 @@
 package com.kingkong.leetcode.LowestCommonAncestorOfABinaryTree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public class TreeNode {
         int val;
@@ -8,7 +11,38 @@ public class Solution {
         TreeNode(int x) { val = x; }
     }
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    private boolean search(TreeNode root, TreeNode node, List<TreeNode> path) {
+        if (root == null) {
+            return false;
+        }
+
+        path.add(root);
+
+        if (root == node) {
+            return true;
+        }
+        if (search(root.left, node, path) || search(root.right, node, path)) {
+            return true;
+        }
+        path.remove(path.size() - 1);
+        return false;
+    }
+
+    public TreeNode lowestCommonAncestor01(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> ppath = new ArrayList<>();
+        List<TreeNode> qpath = new ArrayList<>();
+
+        search(root, p, ppath);
+        search(root, q, qpath);
+
+        int i = 0, len = Math.min(ppath.size(), qpath.size());
+        while (i < len && ppath.get(i) == qpath.get(i)) {
+            ++i;
+        }
+        return ppath.get(i-1);
+    }
+
+    public TreeNode lowestCommonAncestor02(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) {
             return root;
         }
